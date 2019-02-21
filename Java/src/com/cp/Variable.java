483dca5a36e1f2d570354db8579becd2ddbc343c
Hi <Hi@Hi-PC>
2019-02-21 10:59:45 +0530

    final commit from java

---
+++ Java/src/com/cp/Variable.java
@@ -0,0 +1,10 @@
+package com.cp;
+
+public class Variable {
+
+	public static void main(String[] args) {
+			int $hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh456555 = 10;
+			System.out.println($hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh456555);
+	}
+
+}
